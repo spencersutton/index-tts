7ff6ae53d20b23024f2df3b394343cfc675bcfba
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-10-06 13:24:28 -0400

    cleanup

--- ptest/test_main.py
+++ ptest/test_main.py
@@ -14,11 +14,29 @@ def test_main_entry_point_runs(monkeypatch, tmp_path):
         use_cuda_kernel=False,
         use_deepspeed=False,
     )
-
-    text = "Hi!"
+    text = "Translate for me, what is a surprise!"
     tts.infer(
-        spk_audio_prompt="/Users/spencer/projects/ai-scripts/catherine_laura_bailey_sample.mp3",
+        spk_audio_prompt="examples/voice_01.wav",
         text=text,
-        output_path="outputs/gen.wav",
+        output_path="gen.wav",
+        verbose=True,
+    )
+
+    text = "酒楼丧尽天良，开始借机竞拍房间，哎，一群蠢货。"
+    tts.infer(
+        spk_audio_prompt="examples/voice_07.wav",
+        text=text,
+        output_path="gen.wav",
+        emo_audio_prompt="examples/emo_sad.wav",
+        verbose=True,
+    )
+
+    text = "酒楼丧尽天良，开始借机竞拍房间，哎，一群蠢货。"
+    tts.infer(
+        spk_audio_prompt="examples/voice_07.wav",
+        text=text,
+        output_path="gen.wav",
+        emo_audio_prompt="examples/emo_sad.wav",
+        emo_alpha=0.9,
         verbose=True,
     )

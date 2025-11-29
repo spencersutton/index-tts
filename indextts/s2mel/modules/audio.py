3f2d8d048b127b9b0bf674ed9f27efaffee22970
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-11-29 11:33:54 -0500

    Remove unused code

--- indextts/s2mel/modules/audio.py
+++ indextts/s2mel/modules/audio.py
@@ -1,14 +1,6 @@
 import numpy as np
 import torch
 from librosa.filters import mel as librosa_mel_fn
-from scipy.io.wavfile import read
-
-MAX_WAV_VALUE = 32768.0
-
-
-def load_wav(full_path):
-    sampling_rate, data = read(full_path)
-    return data, sampling_rate
 
 
 def dynamic_range_compression(x, C=1, clip_val=1e-5):
@@ -29,11 +21,6 @@ def dynamic_range_decompression_torch(x, C=1):
 
 def spectral_normalize_torch(magnitudes):
     output = dynamic_range_compression_torch(magnitudes)
-    return output
-
-
-def spectral_de_normalize_torch(magnitudes):
-    output = dynamic_range_decompression_torch(magnitudes)
     return output
 
 

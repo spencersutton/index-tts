1b39d1988f1e306b3bb62f0f974d66f52fb17670
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-11-30 01:54:32 -0500

    load_checkpoint

--- indextts/s2mel/modules/commons.py
+++ indextts/s2mel/modules/commons.py
@@ -76,9 +76,7 @@ class MyModel(nn.Module):
 
 def load_checkpoint2(
     model,
-    optimizer,
     path,
-    load_only_params=True,
     ignore_modules=[],
     is_distributed=False,
     load_ema=False,
@@ -115,14 +113,4 @@ def load_checkpoint2(
             model.models[key].load_state_dict(filtered_state_dict, strict=False)
     model.eval()
 
-    if not load_only_params:
-        epoch = state["epoch"] + 1
-        iters = state["iters"]
-        optimizer.load_state_dict(state["optimizer"])
-        optimizer.load_scheduler_state_dict(state["scheduler"])
-
-    else:
-        epoch = 0
-        iters = 0
-
-    return model, optimizer, epoch, iters
+    return model

e632b949a54a5209c6564bffd5c6660991ef79b2
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-12-02 17:43:02 -0500

    Add parameter types

--- indextts/s2mel/modules/encodec.py
+++ indextts/s2mel/modules/encodec.py
@@ -207,7 +207,7 @@ class SConv1d(nn.Module):
         self.causal = causal
         self.pad_mode = pad_mode
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         _B, _C, _T = x.shape
         kernel_size = self.conv.conv.kernel_size[0]
         stride = self.conv.conv.stride[0]

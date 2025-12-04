87388d793faa37c7374ae926935b4b3c7fee34b6
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-12-04 15:46:08 -0500

    Reformat typings

---
+++ typings/dac/__init__.pyi
@@ -0,0 +1,7 @@
+import audiotools
+
+from . import model, nn, utils
+from .model import DAC, DACFile
+
+__version__ = ...
+__model_version__ = ...
---
+++ typings/dac/model/__init__.pyi
@@ -0,0 +1,3 @@
+from .base import CodecMixin, DACFile
+from .dac import DAC
+from .discriminator import Discriminator

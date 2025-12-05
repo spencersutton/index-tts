23f84bccff40ab4d0dda6c60b918bb6346bbd3ca
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-12-05 13:12:44 -0500

    Use torchcodec to load/save

--- indextts/infer_v2.py
+++ indextts/infer_v2.py
@@ -15,6 +15,8 @@ import safetensors.torch
 import torch
 import torchaudio
 from huggingface_hub import hf_hub_download
+from torchcodec.decoders import AudioDecoder, AudioStreamMetadata
+from torchcodec.encoders import AudioEncoder
 
 from indextts.config import CheckpointsConfig
 
@@ -320,7 +322,12 @@ class IndexTTS2:
             self.gr_progress(value, desc=desc)
 
     def _load_and_cut_audio(self, audio_path, max_audio_length_seconds, verbose=False, sr=None):
-        audio, orig_sr = torchaudio.load(audio_path)
+        decoder = AudioDecoder(audio_path)
+        assert isinstance(decoder.metadata, AudioStreamMetadata)
+        orig_sr = decoder.metadata.sample_rate
+        assert orig_sr is not None
+        audio = decoder.get_all_samples().data
+
         # Convert to mono if stereo
         if audio.shape[0] > 1:
             audio = audio.mean(dim=0, keepdim=True)
@@ -744,7 +751,6 @@ class IndexTTS2:
                     bigvgan_time += time.perf_counter() - m_start_time
                     wav = wav.squeeze(1)
 
-                wav = torch.clamp(32767 * wav, -32767.0, 32767.0)
                 if verbose:
                     print(f"wav shape: {wav.shape}", "min:", wav.min(), "max:", wav.max())
                 wavs.append(wav.cpu())  # to cpu before saving
@@ -778,7 +784,13 @@ class IndexTTS2:
                 print(">> remove old wav file:", output_path)
             if Path(output_path).parent != "":
                 Path(output_path).parent.mkdir(exist_ok=True, parents=True)
-            torchaudio.save(output_path, wav.type(torch.int16), sampling_rate)
+
+            assert wav.dtype == torch.float32
+            assert wav.ndim == 2
+
+            encoder = AudioEncoder(wav, sample_rate=sampling_rate)
+            encoder.to_file(output_path)
+
             print(">> wav file saved to:", output_path)
             if stream_return:
                 return None
@@ -787,7 +799,8 @@ class IndexTTS2:
             if stream_return:
                 return None
             # 返回以符合Gradio的格式要求
-            wav_data = wav.type(torch.int16)
+            # Scale to int16 range for Gradio compatibility
+            wav_data = (wav * torch.iinfo(torch.int16).max).type(torch.int16)
             wav_data = wav_data.numpy().T
             yield (sampling_rate, wav_data)
 

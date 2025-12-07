1bb6b11d61ddde71f786309bc9125f4963fe5ab1
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-12-06 19:32:35 -0500

    Remove unused

--- indextts/utils/maskgct/models/codec/amphion_codec/quantize/__init__.py
+++ indextts/utils/maskgct/models/codec/amphion_codec/quantize/__init__.py
@@ -3,16 +3,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+
 from indextts.utils.maskgct.models.codec.amphion_codec.quantize.factorized_vector_quantize import (
     FactorizedVectorQuantize,
 )
-from indextts.utils.maskgct.models.codec.amphion_codec.quantize.lookup_free_quantize import LookupFreeQuantize
 from indextts.utils.maskgct.models.codec.amphion_codec.quantize.residual_vq import ResidualVQ
-from indextts.utils.maskgct.models.codec.amphion_codec.quantize.vector_quantize import VectorQuantize
 
 __all__ = [
     "FactorizedVectorQuantize",
-    "LookupFreeQuantize",
     "ResidualVQ",
-    "VectorQuantize",
 ]

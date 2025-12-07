a5a0f507ee1916dd585a5a814003663167dd0827
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-12-06 19:32:35 -0500

    Remove unused

--- indextts/utils/maskgct/models/codec/amphion_codec/quantize/__init__.py
+++ indextts/utils/maskgct/models/codec/amphion_codec/quantize/__init__.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+
 from indextts.utils.maskgct.models.codec.amphion_codec.quantize.factorized_vector_quantize import (
     FactorizedVectorQuantize as FactorizedVectorQuantize,
 )

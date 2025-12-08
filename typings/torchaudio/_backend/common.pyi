7e15c152029bb94d503f4e5aeef81b1870dff615
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-12-08 12:31:32 -0500

    Cleanup torchaudio typings

--- typings/torchaudio/_backend/common.pyi
+++
@@ -1,9 +0,0 @@
-class AudioMetaData:
-    def __init__(
-        self,
-        sample_rate: int,
-        num_frames: int,
-        num_channels: int,
-        bits_per_sample: int,
-        encoding: str,
-    ) -> None: ...

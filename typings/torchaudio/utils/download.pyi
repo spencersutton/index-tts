3fb7fcf66923cc54ddd6717ac53d43c76aafeddb
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-12-12 01:50:28 -0500

    typings

--- typings/torchaudio/utils/download.pyi
+++ typings/torchaudio/utils/download.pyi
@@ -1,14 +1,1 @@
-from os import PathLike
-
-from torchaudio._internal.module_utils import dropping_support
-
 _LG = ...
-
-@dropping_support
-def download_asset(
-    key: str,
-    hash: str = ...,
-    path: str | PathLike = ...,
-    *,
-    progress: bool = ...,
-) -> str: ...

e84c1a30358a3795a2b8ebf94e2257e8ae890bf2
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-12-20 15:29:01 -0500

    Ruff fixes (PLW)

--- indextts/gpt/transformers_modeling_utils.py
+++ indextts/gpt/transformers_modeling_utils.py
@@ -314,8 +314,7 @@ def get_state_dict_dtype(state_dict):
             return t.dtype
 
     # if no floating dtype was found return whatever the first dtype is
-    else:
-        return next(state_dict.values()).dtype
+    return next(state_dict.values()).dtype
 
 
 def dtype_byte_size(dtype):

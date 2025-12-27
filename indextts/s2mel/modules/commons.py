4a0af0888201cd3d730a3051f220a50df3b57e28
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-12-27 02:17:41 -0500

    Refactor fused_add_tanh_sigmoid_multiply function: simplify parameter definition

--- indextts/s2mel/modules/commons.py
+++ indextts/s2mel/modules/commons.py
@@ -3,10 +3,7 @@ from torch import Tensor
 
 
 @torch.compile(dynamic=True)
-def fused_add_tanh_sigmoid_multiply(
-    input_a: Tensor,
-    input_b: Tensor,
-) -> Tensor:
+def fused_add_tanh_sigmoid_multiply(input_a: Tensor, input_b: Tensor) -> Tensor:
     in_act = input_a + input_b
     t_act_part, s_act_part = torch.chunk(in_act, 2, dim=1)
     t_act = torch.tanh(t_act_part)

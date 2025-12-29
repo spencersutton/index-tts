67cae4ecb865545cccb132829ce7b29e97cd836a
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-12-29 04:27:13 -0500

    Refactor: simplify fused_add_tanh_sigmoid_multiply by inlining operations

--- indextts/s2mel/modules/commons.py
+++ indextts/s2mel/modules/commons.py
@@ -4,8 +4,5 @@ from torch import Tensor
 
 @torch.compile(dynamic=True)  # pyright: ignore[reportUntypedFunctionDecorator]
 def fused_add_tanh_sigmoid_multiply(input_a: Tensor, input_b: Tensor) -> Tensor:
-    in_act = input_a + input_b
-    t_act_part, s_act_part = torch.chunk(in_act, 2, dim=1)
-    t_act = torch.tanh(t_act_part)
-    s_act = torch.sigmoid(s_act_part)
-    return t_act * s_act
+    t_act_part, s_act_part = torch.chunk(input_a + input_b, 2, dim=1)
+    return torch.tanh(t_act_part) * torch.sigmoid(s_act_part)

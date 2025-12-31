f3c86219ec4a6fb138b631a3762478e687685279
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-12-31 18:12:32 -0500

    Refactor: move _returns_nn_module_call definition inside patch_call and improve type hints

--- indextts/util.py
+++ indextts/util.py
@@ -1,11 +1,13 @@
+import functools
 from collections.abc import Callable
+from typing import Any
 
 from torch import nn
 
 
-def _returns_nn_module_call(*_args: object):  # noqa: ANN202
-    return nn.Module.__call__
-
-
 def patch_call[**P, R](_src_func: Callable[P, R]) -> Callable[..., Callable[P, R]]:
+    @functools.wraps(_src_func)
+    def _returns_nn_module_call(*_args: object) -> Any:
+        return nn.Module.__call__
+
     return _returns_nn_module_call

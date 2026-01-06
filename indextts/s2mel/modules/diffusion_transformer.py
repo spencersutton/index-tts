fix: update weight normalization to use parametrizations and modify related checkpoints

indextts/s2mel/modules/diffusion_transformer.py
@@ -2,7 +2,7 @@ import math
 
 import torch
 from torch import nn
-from torch.nn.utils import weight_norm
+from torch.nn.utils.parametrizations import weight_norm
 
 from indextts.s2mel.modules.commons import sequence_mask
 from indextts.s2mel.modules.gpt_fast.model import ModelArgs, Transformer

indextts/s2mel/modules/encodec.py
@@ -14,7 +14,8 @@ import einops
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch.nn.utils import spectral_norm, weight_norm
+from torch.nn.utils import spectral_norm
+from torch.nn.utils.parametrizations import weight_norm
 
 
 class ConvLayerNorm(nn.LayerNorm):
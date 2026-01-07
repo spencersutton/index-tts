299e5909cf784825e436925b95aa6522781d1930
Spencer Sutton <spencer.e.sutton@gmail.com>
2026-01-07 17:14:01 -0500

    refactor: remove unused functions from commons.py

--- indextts/s2mel/modules/commons.py
+++ indextts/s2mel/modules/commons.py
@@ -10,7 +10,8 @@ def fused_add_tanh_sigmoid_multiply(input_a, input_b, n_channels):
     t_act_part, s_act_part = torch.split(in_act, n_channels_int, dim=1)
     t_act = torch.tanh(t_act_part)
     s_act = torch.sigmoid(s_act_part)
-    return t_act * s_act
+    acts = t_act * s_act
+    return acts
 
 
 def sequence_mask(length, max_length=None):
@@ -44,7 +45,8 @@ class MyModel(nn.Module):
         })
 
     def forward(self, x, target_lengths, prompt_len, cond, y):
-        return self.models["cfm"](x, target_lengths, prompt_len, cond, y)
+        x = self.models["cfm"](x, target_lengths, prompt_len, cond, y)
+        return x
 
     def enable_torch_compile(self):
         """Enable torch.compile optimization.

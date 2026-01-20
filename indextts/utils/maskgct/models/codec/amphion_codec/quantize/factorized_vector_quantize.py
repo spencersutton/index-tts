2e2e610ffb170adcd1fc11166d0a42e056a2982a
Spencer Sutton <spencer.e.sutton@gmail.com>
2026-01-20 12:55:03 -0500

    refactor: remove unused functions/classes

--- indextts/utils/maskgct/models/codec/amphion_codec/quantize/factorized_vector_quantize.py
+++ indextts/utils/maskgct/models/codec/amphion_codec/quantize/factorized_vector_quantize.py
@@ -14,10 +14,6 @@ def WNConv1d(*args, **kwargs):
     return weight_norm(nn.Conv1d(*args, **kwargs))
 
 
-def WNConvTranspose1d(*args, **kwargs):
-    return weight_norm(nn.ConvTranspose1d(*args, **kwargs))
-
-
 class FactorizedVectorQuantize(nn.Module):
     def __init__(
         self, input_dim, codebook_size, codebook_dim, commitment=0.005, codebook_loss_weight=1.0, use_l2_normlize=True

5317ef51c7514b9aab1ba25e1d08215f4c0e875d
Spencer Sutton <spencer.e.sutton@gmail.com>
2026-01-20 14:17:04 -0500

    dev: add sync script for file synchronization

---
+++ sync.sh
@@ -0,0 +1,17 @@
+set -euo pipefail
+
+LOCAL_DIR=~/projects/index-tts/
+REMOTE_DIR='~/index-tts/'
+
+REMOTE_HOST='Spencer-Desktop'
+REMOTE_TARGET=${REMOTE_HOST}:${REMOTE_DIR}
+
+UV_CMD='~/.local/bin/uv'
+TEXT="This is a test."
+VOICE_FILE='outputs/mizora.ogg'
+OUTPUT_FILE='outputs/gen.wav'
+
+rsync --info=NAME -rc -C --exclude='.serena' -f ':- .gitignore' ${LOCAL_DIR} ${REMOTE_TARGET}
+ssh -t ${REMOTE_HOST} "time ${UV_CMD} run --directory ${REMOTE_DIR} indextts/cli.py -v ${VOICE_FILE} '${TEXT}' --force -o ${OUTPUT_FILE}"
+mkdir -p ${LOCAL_DIR}/outputs
+rsync --info=NAME -a ${REMOTE_TARGET}/outputs/gen.wav ${LOCAL_DIR}/outputs/gen.wav

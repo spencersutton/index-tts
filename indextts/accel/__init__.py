77abe91d80873a17bd39b5a36abea5bbdefb4161
Spencer Sutton <spencer.e.sutton@gmail.com>
2026-01-21 00:30:18 -0500

    refactor: clean up imports and remove unused code in accel and infer_v2 modules

--- indextts/accel/__init__.py
+++ indextts/accel/__init__.py
@@ -1,9 +1,4 @@
-from indextts.accel.accel_engine import AccelInferenceEngine  # noqa: F401
-from indextts.accel.attention import (  # noqa: F401
-    Attention,
-    get_forward_context,
-    reset_forward_context,
-    set_forward_context,
-)
-from indextts.accel.gpt2_accel import GPT2AccelAttention, GPT2AccelModel  # noqa: F401
-from indextts.accel.kv_manager import KVCacheManager, Seq  # noqa: F401
+from indextts.accel.accel_engine import AccelInferenceEngine
+from indextts.accel.gpt2_accel import GPT2AccelModel
+
+__all__ = ["AccelInferenceEngine", "GPT2AccelModel"]

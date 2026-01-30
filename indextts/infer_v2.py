76e436fc0edce4f984b45de1682c951d90572600
Spencer Sutton <spencer.e.sutton@gmail.com>
2026-01-30 01:54:33 -0500

    fix: ensure self.device is str for autocast

--- indextts/infer_v2.py
+++ indextts/infer_v2.py
@@ -86,7 +86,7 @@ def _load_and_cut_audio(audio_path: Path, sample_rate: float | None = None) -> t
 class IndexTTS2:
     cfg: IndexTTSConfig
     dtype: torch.dtype
-    device: str
+    device: torch.device
     use_fp16: bool
     use_cuda_kernel: bool
     use_accel: bool
@@ -307,7 +307,9 @@ class IndexTTS2:
             use_torch_compile (bool): whether to use torch.compile for optimization or not.
         """
 
-        self.device = str(device or torch.accelerator.current_accelerator() or torch.get_default_device())
+        self.device = (
+            torch.device(device) if device else torch.accelerator.current_accelerator() or torch.get_default_device()
+        )
         self.use_cuda_kernel = use_cuda_kernel and self.device.startswith("cuda")
         self.use_fp16 = use_fp16 and self.device not in ["cpu", "mps"]
         self.cfg = IndexTTSConfig()
@@ -533,7 +535,7 @@ class IndexTTS2:
             text_tokens = torch.tensor(text_tokens, dtype=torch.int32, device=self.device).unsqueeze(0)
 
             with torch.inference_mode():
-                with torch.autocast(text_tokens.device.type, dtype=self.dtype), gpt_gen_time:
+                with torch.autocast(self.device.type, dtype=self.dtype), gpt_gen_time:
                     emotion_vector = self.gpt.get_emo_vec(emotion_conditioning_embedding)
                     base_vector = self.gpt.get_emo_vec(speaker_conditioning_embedding)
 
@@ -575,7 +577,7 @@ class IndexTTS2:
                 ]
                 codes = codes[:, : max(code_lens)]
 
-                with torch.autocast(self.device, dtype=self.dtype), gpt_forward_time:
+                with torch.autocast(self.device.type, dtype=self.dtype), gpt_forward_time:
                     assert speech_conditioning_latent.shape == torch.Size([1, 32, 1280])
                     assert text_tokens.shape[0] == 1
                     assert codes.shape[0] == 1

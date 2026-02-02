5acea9eeed7570ea14563ef31f238e04a07e0b6e
Spencer Sutton <spencer.e.sutton@gmail.com>
2026-02-01 21:34:32 -0500

    refactor: streamline tensor manipulation in DiT class by chaining method calls

--- indextts/s2mel/modules/diffusion_transformer.py
+++ indextts/s2mel/modules/diffusion_transformer.py
@@ -144,8 +144,7 @@ class DiT(nn.Module):
         x_res = self.transformer.__call__(x_in, t1.unsqueeze(1), input_pos)
 
         x_res = self.skip_linear(torch.cat([x_res, x], dim=-1))
-        x = self.conv1.__call__(x_res)
-        x = x.mT
+        x = self.conv1.__call__(x_res).mT
         t2 = self.t_embedder2(t)
 
         x = self.wavenet.__call__(x, g=t2.unsqueeze(2)).mT + self.res_projection(x_res)

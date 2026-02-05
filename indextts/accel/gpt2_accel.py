3096bd7ce4b2a48b4876a0ec3a39cec47e4f929b
Spencer Sutton <spencer.e.sutton@gmail.com>
2026-02-04 23:33:34 -0500

    refactor: adjust import statements and clean up unused code in attention and gpt2_accel modules

--- indextts/accel/gpt2_accel.py
+++ indextts/accel/gpt2_accel.py
@@ -104,16 +104,12 @@ class _GPT2AccelAttention(nn.Module):
 
         # Reshape back: [B*T, H, D] -> [B, H, T, D]
         attn_output = o_flat.view(bsz, seq_len, num_heads, head_dim).transpose(1, 2)
-
         attn_output = self._merge_heads(attn_output, self.num_heads, self.head_dim)
-
         attn_output = self.c_proj(attn_output)
 
-        outputs = (attn_output, None)
         if output_attentions:
-            outputs += (None,)
-
-        return outputs
+            return (attn_output, None, None)
+        return (attn_output, None)
 
     def _split_heads(self, tensor: Tensor, num_heads: int, head_dim: int) -> Tensor:
         new_shape = (*tensor.size()[:-1], num_heads, head_dim)

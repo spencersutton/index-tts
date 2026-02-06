cdae2e73b89677865bc87865325935fc98f0cad9
Spencer Sutton <spencer.e.sutton@gmail.com>
2026-02-06 14:08:08 -0500

    feat: make SnakeBeta a subclass of Snake

--- src/bigvgan/activations.py
+++ src/bigvgan/activations.py
@@ -57,6 +57,12 @@ class Snake(nn.Module):
 
         self.no_div_by_zero = 0.000000001
 
+    def _parameter(self) -> Tensor:
+        alpha = self.alpha.unsqueeze(0).unsqueeze(-1)  # Line up with x to [B, C, T]
+        if self.alpha_logscale:
+            alpha = torch.exp(alpha)
+        return alpha
+
     @override
     def forward(self, x: Tensor) -> Tensor:
         """
@@ -67,10 +73,10 @@ class Snake(nn.Module):
         alpha = self.alpha.unsqueeze(0).unsqueeze(-1)  # Line up with x to [B, C, T]
         if self.alpha_logscale:
             alpha = torch.exp(alpha)
-        return x + (1.0 / (alpha + self.no_div_by_zero)) * pow(sin(x * alpha), 2)
+        return x + (1.0 / (self._parameter() + self.no_div_by_zero)) * pow(sin(x * alpha), 2)
 
 
-class SnakeBeta(nn.Module):
+class SnakeBeta(Snake):
     """
     A modified Snake function which uses separate parameters for the magnitude of the periodic components
     Shape:
@@ -89,11 +95,7 @@ class SnakeBeta(nn.Module):
         >>> x = a1(x)
     """
 
-    in_features: int
-    alpha_logscale: bool
-    alpha: Parameter
     beta: Parameter
-    no_div_by_zero: float
 
     def __init__(
         self, in_features: int, alpha: float = 1.0, alpha_trainable: bool = True, alpha_logscale: bool = False
@@ -108,33 +110,15 @@ class SnakeBeta(nn.Module):
             beta is initialized to 1 by default, higher values = higher-magnitude.
             alpha will be trained along with the rest of your model.
         """
-        super().__init__()
-        self.in_features = in_features
+        super().__init__(in_features, alpha, alpha_trainable, alpha_logscale)
 
-        # Initialize alpha
-        self.alpha_logscale = alpha_logscale
-        if self.alpha_logscale:  # Log scale alphas initialized to zeros
-            self.alpha = Parameter(torch.zeros(in_features) * alpha)
-            self.beta = Parameter(torch.zeros(in_features) * alpha)
-        else:  # Linear scale alphas initialized to ones
-            self.alpha = Parameter(torch.ones(in_features) * alpha)
-            self.beta = Parameter(torch.ones(in_features) * alpha)
-
-        self.alpha.requires_grad = alpha_trainable
+        # Initialize beta
+        self.beta = Parameter(self.alpha.detach().clone())
         self.beta.requires_grad = alpha_trainable
 
-        self.no_div_by_zero = 0.000000001
-
     @override
-    def forward(self, x: Tensor) -> Tensor:
-        """
-        Forward pass of the function.
-        Applies the function to the input elementwise.
-        SnakeBeta ∶= x + 1/b * sin^2 (xa)
-        """
-        alpha = self.alpha.unsqueeze(0).unsqueeze(-1)  # Line up with x to [B, C, T]
+    def _parameter(self) -> Tensor:
         beta = self.beta.unsqueeze(0).unsqueeze(-1)
         if self.alpha_logscale:
-            alpha = torch.exp(alpha)
             beta = torch.exp(beta)
-        return x + (1.0 / (beta + self.no_div_by_zero)) * pow(sin(x * alpha), 2)
+        return beta
